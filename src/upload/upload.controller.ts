dfa7c0026fb6293da9817f836dc897648911b00a
Rafael Bissani <negociosbissani@gmail.com>
2024-09-21 18:19:43 -0300

    sucesso ao receber imagens da outra API

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,9 +1,9 @@
 import {
-    Controller,
-    Post,
-    UploadedFile,
-    UseInterceptors,
-    BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  BadRequestException,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
@@ -12,46 +12,46 @@ import { UploadService } from './upload.service';
 
 @Controller('upload')
 export class UploadController {
-    constructor(private readonly uploadService: UploadService) { }
+  constructor(private readonly uploadService: UploadService) {}
 
-    @Post('file')
-    @UseInterceptors(
-        FileInterceptor('file', {
-            storage: diskStorage({
-                destination: process.cwd() + '/uploads',
-                filename: (req, file, cb) => {
-                    const randomName = Array(32)
-                        .fill(null)
-                        .map(() => Math.round(Math.random() * 16).toString(16))
-                        .join('');
-                    return cb(null, `${randomName}${extname(file.originalname)}`);
-                },
-            }),
-            limits: {
-                fileSize: 250 * 1024, // Limite de 250KB
-            },
-            fileFilter: (req, file, cb) => {
-                if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
-                    return cb(
-                        new BadRequestException(
-                            'Somente arquivos de imagem são permitidos!',
-                        ),
-                        false,
-                    );
-                }
-                cb(null, true);
-            },
-        }),
-    )
-    async uploadFile(@UploadedFile() file: Express.Multer.File) {
-        if (!file) {
-            throw new BadRequestException('Nenhum arquivo foi enviado.');
+  @Post('file')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: process.cwd() + '/uploads',
+        filename: (req, file, cb) => {
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+          return cb(null, `${randomName}${extname(file.originalname)}`);
+        },
+      }),
+      limits: {
+        fileSize: 250 * 1024, // Limite de 250KB
+      },
+      fileFilter: (req, file, cb) => {
+        if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+          return cb(
+            new BadRequestException(
+              'Somente arquivos de imagem são permitidos!',
+            ),
+            false,
+          );
         }
-
-        console.log('File received:', file); // Verifica se o arquivo foi recebido
-        return {
-            message: 'Arquivo enviado com sucesso!',
-            file,
-        };
+        cb(null, true);
+      },
+    }),
+  )
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('Nenhum arquivo foi enviado.');
     }
+
+    console.log('File received:', file); // Verifica se o arquivo foi recebido
+    return {
+      message: 'Arquivo enviado com sucesso!',
+      file,
+    };
+  }
 }
